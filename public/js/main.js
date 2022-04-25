26c7e077cb2433d1b792db09c855658e5c0e0064
Cassia Sotolani <ca.sotolani@gmail.com>
2022-04-25 16:14:44 -0400

    Atualizando código, criando functions nominais em vez das anonimas, para trabalhar com reuso

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,51 @@
-var frase = $('.frase').text();
-
-var numeroPalavras = frase.split(' ').length;
-
-var tamanhoFrase = $('#tamanho-frase');
-tamanhoFrase.text(numeroPalavras);
-
+var tempoInicial = $('#tempo').text();
 var campoDigitacao = $('.campo-digitacao');
-campoDigitacao.on('input', function() {
-    var conteudo = campoDigitacao.val();
 
-    var quantidadePalavras = conteudo.split(/\S+/).length - 1;
-    $('#contador-palavras').text(quantidadePalavras);
-
-    var quantidadeCaracteres = conteudo.length;
-    $('#contador-caracteres').text(quantidadeCaracteres);
+$(document).ready(function() {
+    atualizarTamanhoFrase();
+    inicializarContadores();
+    inicializarCronometro();
+    $('#botao-reiniciar').click(reiniciarJogo);
 });
 
-var tempoRestante = $('#tempo').text();
-campoDigitacao.one('focus', function() {
-    var cronometroID = setInterval(function() {
-        tempoRestante--;
-        $('#tempo').text(tempoRestante);
-        if(tempoRestante < 1) {
-            campoDigitacao.attr('disabled', true);
-            clearInterval(cronometroID);
-        }
-    }, 1000);
-})
+function atualizarTamanhoFrase() {
+    var frase = $('.frase').text();
+    var numeroPalavras = frase.split(' ').length;
+    var tamanhoFrase = $('#tamanho-frase');
+    tamanhoFrase.text(numeroPalavras);
+}
+
+function inicializarContadores() {
+    campoDigitacao.on('input', function() {
+        var conteudo = campoDigitacao.val();
+    
+        var quantidadePalavras = conteudo.split(/\S+/).length - 1;
+        $('#contador-palavras').text(quantidadePalavras);
+    
+        var quantidadeCaracteres = conteudo.length;
+        $('#contador-caracteres').text(quantidadeCaracteres);
+    });
+}
+
+function inicializarCronometro() {
+    var tempoRestante = $('#tempo').text();
+    campoDigitacao.one('focus', function() {
+        var cronometroID = setInterval(function() {
+            tempoRestante--;
+            $('#tempo').text(tempoRestante);
+            if(tempoRestante < 1) {
+                campoDigitacao.attr('disabled', true);
+                clearInterval(cronometroID);
+            }
+        }, 1000);
+    })
+}
+
+function reiniciarJogo() {
+    campoDigitacao.attr('disabled', false);
+    campoDigitacao.val('');
+    $('#contador-palavras').text('0');
+    $('#contador-caracteres').text('0');
+    $('#tempo').text(tempoInicial);
+    inicializarCronometro();
+}
