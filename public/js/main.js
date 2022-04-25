562c94c82fa9e798eeb521a2c02b1737808240d2
Cassia Sotolani <ca.sotolani@gmail.com>
2022-04-25 15:46:06 -0400

    Melhorando nosso código, implementando functions para criar cronometro decrescente e captua de caracteres

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,29 @@
 var frase = $('.frase').text();
-var numeroPalavras = frase.split(' ').length;
 
-console.log(frase);
-console.log(numeroPalavras);
+var numeroPalavras = frase.split(' ').length;
 
 var tamanhoFrase = $('#tamanho-frase');
 tamanhoFrase.text(numeroPalavras);
+
+var campoDigitacao = $('.campo-digitacao');
+campoDigitacao.on('input', function() {
+    var conteudo = campoDigitacao.val();
+
+    var quantidadePalavras = conteudo.split(/\S+/).length - 1;
+    $('#contador-palavras').text(quantidadePalavras);
+
+    var quantidadeCaracteres = conteudo.length;
+    $('#contador-caracteres').text(quantidadeCaracteres);
+});
+
+var tempoRestante = $('#tempo').text();
+campoDigitacao.one('focus', function() {
+    var cronometroID = setInterval(function() {
+        tempoRestante--;
+        $('#tempo').text(tempoRestante);
+        if(tempoRestante < 1) {
+            campoDigitacao.attr('disabled', true);
+            clearInterval(cronometroID);
+        }
+    }, 1000);
+})
