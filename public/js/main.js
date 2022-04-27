968cbf07b33951fe08e2af5730f4dd3845d87c5a
Cassia Sotolani <ca.sotolani@gmail.com>
2022-04-27 15:06:57 -0400

    Criando function remover linha

--- public/js/main.js
+++ public/js/main.js
@@ -68,20 +68,34 @@ function inserirPlacar() {
     var corpoTabela = $('.placar').find('tbody');
     var usuario = 'Cassia';
     var numPalavras = $('#contador-palavras').text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>"
 
-    var linha = '<tr>' +
-                    '<td>' + usuario + '</td>' +
-                    '<td>' + numPalavras + '</td>' +
-                    '<td>' + botaoRemover + '</td>' +
-                '</tr>';
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find('.botao-remover').click(removerLinha);
+
     corpoTabela.prepend(linha);
 }
 
-$('.botao-remover').on('click', function(event) {
+function novaLinha(usuario, palavras) {
+    var linha = $('<tr>');
+    var colunaUsuario = $('<td>').text(usuario);
+    var colunaPalavras = $('<td>').text(palavras);
+    var colunaRemover = $('<td>');
+    var link = $('<a>').addClass('botao-remover').attr('href', '#');
+    var icone = $('<i>').addClass('small').addClass('material-icons').text('delete');
+
+    link.append(icone);
+    colunaRemover.append(link);
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removerLinha(event) {
     event.preventDefault();
     $(this).parent().parent().remove();
-})
+}
 
 function reiniciarJogo() {
     campoDigitacao.attr('disabled', false);
